143867e508509ca1672d4a0fb52c290ee09d3365
zlatin <post2007@mail.bg>
2017-11-05 11:47:07 +0200

    Initial commit  Changes to be committed: 	new file:   LICENSE 	new file:   README.md 	new file:   ZZZClient.java

---
+++ README.md
@@ -0,0 +1,14 @@
+# zzzserver-java-driver
+Java driver for **ZZZ Base** through ZZZ Server
+
+***ZZZClient.java*** - the file with driver function implementation  
+
+**sample usage:**
+```java
+ZZZProgram(
+	"localhost",								// ZZZ Server host
+	3333,										// ZZZ Server port
+	"#[cout;Hello World from ZZZServer!]")		// ZZZ Base sample program
+```
+
+Returned result encoded in the utf-8
---
+++ ZZZClient.java
@@ -0,0 +1,81 @@
+/*
+	ZZZClient.fs
+
+	Copyright 2017 ZZZ Ltd. - Bulgaria
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package zzzclientj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author ZZZ Ltd. - Bulgaria
+ */
+public class MainApp {
+
+    public static String ZZZProgram(String serverHost, int serverPort,
+            String program)
+    {
+        String result = "";
+        
+        try {
+        	if(serverHost.equals("localhost"))
+        		serverHost = "127.0.0.1";
+            Socket socket = new Socket(serverHost, serverPort);
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(
+                    socket.getOutputStream(), "UTF-8"), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            
+            out.print(program + '\0');
+            out.flush();
+            
+            StringBuilder sb = new StringBuilder();
+            int DEFAULT_BUFFER_SIZE = 1000;
+            char buf[] = new char[DEFAULT_BUFFER_SIZE];
+            int n;
+            while ((n = in.read(buf)) > 0) {
+                sb.append(buf, 0, n);
+            }
+            result = sb.toString();
+
+            out.close();
+            in.close();
+            socket.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }    
+        
+        return result;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	long startTime = System.currentTimeMillis();
+    	
+        System.out.println(ZZZProgram("localhost", 3333, "#[cout;Hello world from ZZZServer!]"));
+        
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println(elapsedTime + " milliseconds");
+	}
+}
